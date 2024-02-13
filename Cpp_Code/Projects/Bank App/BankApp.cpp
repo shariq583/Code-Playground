75941e3e311fbfcd2e17bfb5f49c0ff46d87a43a
shariq583 <gazdarshariq0@gmail.com>
2024-02-13 21:21:11 +0500

    BankApp updated!

--- Cpp_Code/Projects/Bank App/BankApp.cpp
+++ Cpp_Code/Projects/Bank App/BankApp.cpp
@@ -1,109 +1,102 @@
 #include <iostream>
 using namespace std;
+class UserInfo;
 class AdminDash;
-class cred
-{
-    protected :
+class cred {
+protected:
     string userPin;
     string adminPin;
     string adminAccNo;
-    public :
+
+public:
     string userAccNo;
-    cred()
-    {
+    cred() {
         userPin = "0000";
         adminPin = "0000";
     }
-    void setUserAccNo(string inpUserAccNo)
-    {
+    void setUserAccNo(string inpUserAccNo) {
         userAccNo = inpUserAccNo;
     }
-    void setUserPin(string inpUserPin)
-    {
+    void setUserPin(string inpUserPin) {
         userPin = inpUserPin;
     }
-    void setAdminAccNo(string inpAdminAccNo)
-    {
+    void setAdminAccNo(string inpAdminAccNo) {
         adminPin = inpAdminAccNo;
     }
-    void setAdminPin(string inpAdminPin)
-    {
+    void setAdminPin(string inpAdminPin) {
         adminPin = inpAdminPin;
     }
-    int UserCheckAccNo (string icheckNo)
-    {
-        if (userAccNo == icheckNo)
-        {
+    int UserCheckAccNo(string icheckNo) {
+        if (userAccNo == icheckNo) {
             return 1;
         }
-        else 
-        {
+        else {
             return 0;
         }
-        
     }
-    int UserCheckPin (string icheckPin)
-    {
-           if (userAccNo == icheckPin)
-        {
+    int UserCheckPin(string icheckPin ) {
+        if (userPin == icheckPin ) {
             return 1;
         }
-        else 
-        {
+        else {
             return 0;
         }
     }
 };
-class AdminDash : public cred , protected UserInfo
-{
-    private :
-    string intrRate="4";
-    protected :
+
+
+class AdminDash : public cred , public UserInfo{
+private:
+    string intrRate = "4";
+
+protected:
     string SavMon;
     string IntrMon;
-    public :
-        void InitMon(int tempMon)
-        {
-            SavMon = tempMon;
-        }
-        void IntrMonSet()
-        {
-            IntrMon = ((stoi(SavMon))+(stoi(intrRate))/(100));
-        }
-        void AdDisplay(cred temp)
-        {
-           std::cout << "The User A/c No. is:" <<temp.userAccNo << std::endl;
-        }
 
-};
-class UserInfo : public cred , protected AdminDash
-{
-    protected :
-    public :
-    
-
-
+public:
+    void AInitMon(int tempMon) {
+        SavMon = to_string(tempMon);
+    }
+    void AIntrMonSet() {
+        IntrMon = to_string(stoi(SavMon) + stoi(intrRate) / 100);
+    }
+    void AdDisplay(cred temp) {
+        std::cout << "The User A/c No. is:" << temp.userAccNo << std::endl;
+    }
 };
 
-int main()
-{
-    // Variables and objects 
+class UserInfo : public cred , public AdminDash{
+    public:
+        void UInitMon (string funcMon)
+        {
+            SavMon = funcMon;
+        }
+};
+
+int main() {
+    // Variables and objects
     // Choice Vars
     int Mchoice;
     int Uchoice;
     int Achoice;
-    //User Vars
+    // User Cred Vars
     string UPin_S;
     string UAccNo_S;
     string UPin_L;
     string UAccNo_L;
     cred Ucred;
-    // Admin Vars
+    UserInfo User ;
+    AdminDash Admin;
+    // User Info Vars
+
+
+    // Admin Cred Vars
     string APin_S;
     string AAccNo_S;
     string APin_L;
     string AAccNo_L;
     cred Acred;
+
 
     std::cout << "=========" << std::endl;
     std::cout << "Bank App" << std::endl;
@@ -114,86 +107,89 @@ int main()
     std::cout << "2.Admin..." << std::endl;
     cin >> Mchoice;
 
-    switch (Mchoice)
-    {
-    case 1: //User Page
+    switch (Mchoice) {
+    case 1: // User Page
         std::cout << "1.Sign Up!" << std::endl;
         std::cout << "2.Log In!" << std::endl;
         cin >> Uchoice;
-        switch (Uchoice)
-            {
-            case 1: //Sign Up
-                cout<<"Enter A Account No.";                
-                cin>>UAccNo_S;
-                Ucred.setUserAccNo(UAccNo_S);
-                cout<<"Set A Account Pin";
-                cin>>UPin_S;
-                Ucred.setUserPin(UPin_S);
-                break;
-            
-            case 2: //Login Page
-            {
-                int tempPL , tempNL;
-                std::cout << "Enter Your account number" << std::endl;
-                cin>>UAccNo_L;
-                tempNL=Ucred.UserCheckAccNo(UAccNo_L);
-                std::cout << "Enter Your account Pin" << std::endl;
-                cin>>UPin_L;
-                tempPL=Ucred.UserCheckAccNo(UPin_L);
-                if (tempPL == 0)
-                {
-                    // code to be put.
-                }
-                else
-                {
-                    /* code */
-                }
-                
-            }
+        switch (Uchoice) {
+        case 1: // Sign Up
+        {
 
-            default: //UChoice
-                std::cout << "Wrong Choice!" << std::endl;
-                break;
-             }
+            cout << "Enter A Account No.";
+            cin >> UAccNo_S;
+            Ucred.setUserAccNo(UAccNo_S);
+            cout << "Set A Account Pin";
+            cin >> UPin_S;
+            Ucred.setUserPin(UPin_S); 
+            goto UserLogin;           
             break;
+        }
+
+        case 2: // Login Page
+        {
+            UserLogin :
+            int tempPL, tempNL;
+            std::cout << "Enter Your account number" << std::endl;
+            cin >> UAccNo_L;
+            tempNL = Ucred.UserCheckAccNo(UAccNo_L);
+            std::cout << "Enter Your account Pin" << std::endl;
+            cin >> UPin_L;
+            tempPL = Ucred.UserCheckPin(UPin_L );
+            if (tempPL == 1) {
+                // code to be put.
+                string tempSav;
+                std::cout << "How much money would you like to deposite ??" ;
+                cin>>tempSav;
+                User.UInitMon(tempSav);
+            }
+            else {
+                /* code */
+                cout<<"Unatherized!";
+            }
+            break;
+        }
+
+        default: // UChoice
+            std::cout << "Wrong Choice!" << std::endl;
+            break;
+        }
+        break;
     case 2: // Admin Page
     {
         std::cout << "1.Sign Up!" << std::endl;
         std::cout << "2.Log In!" << std::endl;
         cin >> Achoice;
-        switch (Achoice)
-            {
-            case 1://Sign up!
-                cout<<"Enter A Account No.";                
-                cin>>AAccNo_S;
-                Acred.setAdminAccNo(AAccNo_S);
-                cout<<"Set A Account Pin";
-                cin>>APin_S;
-                Acred.setUserPin(APin_S);
-                break;
-            
-            case 2://Login 
-            {
-                int tempPLA , tempNLA;
-                std::cout << "Enter Your account number" << std::endl;
-                cin>>AAccNo_L;
-                tempNLA=Acred.UserCheckAccNo(AAccNo_L);
-                std::cout << "Enter Your account Pin" << std::endl;
-                cin>>APin_L;
-                tempPLA=Acred.UserCheckAccNo(APin_L);
-                if (tempPLA == 0)
-                {
-                    // code to be put....
-                }
-                else
-                {
-                    // code to be put...
-                }
-                
-            }
-    }
+        switch (Achoice) {
+        case 1: // Sign up!
+            cout << "Enter A Account No.";
+            cin >> AAccNo_S;
+            Acred.setAdminAccNo(AAccNo_S);
+            cout << "Set A Account Pin";
+            cin >> APin_S;
+            Acred.setAdminPin(APin_S);
+            break;
 
-    default: //Mchoice
+        case 2: // Login
+        {
+            int tempPLA, tempNLA;
+            std::cout << "Enter Your account number" << std::endl;
+            cin >> AAccNo_L;
+            tempNLA = Acred.UserCheckAccNo(AAccNo_L);
+            std::cout << "Enter Your account Pin" << std::endl;
+            cin >> APin_L;
+            tempPLA = Acred.UserCheckPin(APin_L);
+            if (tempPLA == 0) {
+                // code to be put....
+            }
+            else {
+                // code to be put...
+            }
+        }
+        }
+
+    default: // Mchoice
         break;
+    }
     }
 }
