85aad461ed3ca76731b4d18c173a431073be78d5
shariq583 <gazdarshariq0@gmail.com>
2024-02-20 14:51:46 +0500

    derivedClass Construvtor added !

--- Cpp_Code/Classes/Derived_Constructor.cpp
+++ Cpp_Code/Classes/Derived_Constructor.cpp
@@ -1,6 +1,39 @@
 #include<iostream>
 using namespace std;
-int main()
+class Base
 {
 
+    public :
+    int a;
+    Base()
+    {
+        a= 0;
+    }
+    Base(int tempa)
+    {
+        a= tempa;
+    }
+};
+class derived : public Base
+{
+    public :
+    int b ;
+    derived()
+    {
+        b=0;
+    }
+    derived(int tempb)
+    {
+        b=tempb;
+    }
+};
+int main()
+{   
+    derived d(5);
+    std::cout << "The Value of a is :" 
+    << d.a
+    << std::endl;
+    std::cout << "The Value of b is :" 
+    << d.b
+    << std::endl;
 }
