89ed23ec66d620c83c4c553bda76c02f34715cda
shariq583 <gazdarshariq0@gmail.com>
2024-02-21 11:24:22 +0500

    JS Loops and strings

---
+++ Web_development/Html,Css,Js/JavaScript/Apna-College-Course/Loops and strings/script.js
@@ -0,0 +1,13 @@
+// let gameNum = 5 ;
+// let userInp = prompt("Enter The Value");
+// while(gameNum != userInp )
+// {
+//     userInp = prompt("Enter The Value Again");
+// }
+// console.log("You Won!");
+// ${Template litreals}
+let userInpName = prompt("Enter Your Name ");
+let username = "@"+userInpName+userInpName.length;
+console.log(username);
+
+
