14c718ad28b71d389c2618b76a1a88e3003eb6a5
shariq583 <gazdarshariq0@gmail.com>
2024-02-22 10:52:35 +0500

    Array add!

---
+++ Web_development/Html,Css,Js/JavaScript/Apna-College-Course/Arrays/Script.js
@@ -0,0 +1,38 @@
+// var i=1;
+// let Arr = [];
+// for (let index = 1; index <=5; index++) {
+//     Arr.push(index);
+//     console.log(Arr);
+// }
+// for loop 
+// for (let index = 0; index < Arr.length; index++) {
+//     console.log(Arr[index]);
+    
+// }
+
+// // for of loop 
+// for (let el of Arr) {
+//     console.log(el);
+// }
+
+
+// for each loop 
+// Arr.forEach(element => {
+//     console.log(element);
+// });
+
+/* *********************Practise Questions******************* */
+// Qn 1:
+    // let marks = [95 , 65 , 85 , 65 , 42 , 45];
+    // var totMarks= 0 ;
+    // for (let index = 0; index < marks.length; index++) {
+    //     totMarks += marks[index]
+    // }
+    // let avg = totMarks/marks.length;
+    // console.log(`The averrage of ${marks} is: ${avg}%`);
+// Qn 2 :
+    let price =[250,645,300,900,50];
+    for (const item of marks) {
+        price[item]=(item*(10/100));
+        console.log(price);
+    }
