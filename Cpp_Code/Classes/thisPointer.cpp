16782cc26c8ec13ef01a43be8076e3558b679077
shariq583 <gazdarshariq0@gmail.com>
2024-02-24 19:27:35 +0500

    thisPointer add!

---
+++ Cpp_Code/Classes/thisPointer.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+class Base
+{
+    int a;
+    public :
+        void setData(int a)
+        {
+            this->a=a;    
+        }
+        void getData()
+        {
+            std::cout << "The Value of a is " << a << std::endl;
+        }
+};
+int main()
+{
+    Base b;
+    b.setData(3);
+    b.getData();
+}
