ab1ff1d58865de4edb7a43d26b85313d96993cf2
shariq583 <gazdarshariq0@gmail.com>
2024-02-27 20:41:20 +0500

    Clock done!

--- Web_development/Html,Css,Js/JavaScript/JavaScrip Clock/script.js
+++ Web_development/Html,Css,Js/JavaScript/JavaScrip Clock/script.js
@@ -3,7 +3,13 @@ let time;
 let defDate = document.getElementById('deftime')
 setInterval(() => {
     date =  new Date();
-    time= date.getHours()+":"+ date.getMinutes()+":"+date.getSeconds();
-    day= date.toLocaleDateString();
-    defDate.innerText=time+' on '+day    
+    const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    const d = new Date();
+    let month = months[d.getMonth()];
+    time= date.toLocaleString('en-US', { hour: 'numeric', minute: 'numeric',second :'2-digit', hour12: true })
+    day= date.getDay();
+    let Ndate = date.getDate();
+    let year = date.getFullYear();
+    defDate.innerText=`${time}    \n \t Date : ${Ndate}/${month}/${year}`
+ 
 }, 1000);
