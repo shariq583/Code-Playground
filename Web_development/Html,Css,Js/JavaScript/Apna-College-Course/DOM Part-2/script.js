a308345544926d64820bb4a7db10fa49a01a0973
shariq583 <gazdarshariq0@gmail.com>
2024-02-27 11:35:01 +0500

    Dom 1 done 2 half

--- Web_development/Html,Css,Js/JavaScript/Apna-College-Course/DOM Part-2/script.js
+++ Web_development/Html,Css,Js/JavaScript/Apna-College-Course/DOM Part-2/script.js
@@ -1,5 +1,5 @@
 let heading =document.querySelector("h2");
-let str = "Frome Shariq Gazdar";
+let str = "DOM Part-2";
 heading.innerText=(`${heading.innerText} ${str} `);
 let div = document.querySelectorAll(".box");
 idx= 1;
@@ -7,3 +7,10 @@ for (const i of div) {
     i.innerText = `Some unique text for div ${idx}`;
     idx++;
 }
+console.log(`${div[0].getAttribute("class")}`);
+let btn = document.querySelector("#txtCntr");
+btn.addEventListener("click",()=>{
+    for (const i of div) {
+        i.setAttribute("class","txtCntr")
+    }
+})
