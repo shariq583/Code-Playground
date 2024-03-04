10b305b71ae0eb4ede83f83a9ad6188ff43fdf01
shariq583 <gazdarshariq0@gmail.com>
2024-03-04 15:10:02 +0500

    Airmon done!

--- Cpp_Code/Projects/Pasword Dict Tool/Airmon.cpp
+++ Cpp_Code/Projects/Pasword Dict Tool/Airmon.cpp
@@ -6,23 +6,31 @@ int main()
     string InpPass;
     string Pass = "Shariq";
     ifstream filePassIn("rockyou.txt");
+    int sucStat=0;
     while (filePassIn.eof()==0)
     {
-        getline(filePassIn,InpPass);
+     getline(filePassIn,InpPass);
         if (InpPass==Pass)
         {
             std::cout << "The Password is :" << InpPass<<std::endl;
+            sucStat++;
             filePassIn.close();
         }
         else if(InpPass!=Pass && filePassIn.eof()==0)
         {
             std::cout << "Checking..." << std::endl;
         }
-        else if(InpPass!=Pass && filePassIn.eof()!=0)
+        else if(InpPass!=Pass && filePassIn.eof()==0)
         {
             std::cout << "Password Not Found!" << std::endl;
+            break;
         }
     }
+    if (sucStat==0)
+    {
+        std::cout << "Password Not Found!" << std::endl;
+    }
+    
     filePassIn.close();
     return 0;
 }
