f5354d14f79fdad46780187d0f3d8d969e6dddff
shariq583 <gazdarshariq0@gmail.com>
2024-03-08 12:03:54 +0500

    Rock Paper Scissors game done!

--- Web_development/Html,Css,Js/JavaScript/Apna-College-Course/Rock Paper Sicors/script.js
+++ Web_development/Html,Css,Js/JavaScript/Apna-College-Course/Rock Paper Sicors/script.js
@@ -1,19 +1,30 @@
 let gameBtns = document.querySelectorAll(".choice");
 let msgBox = document.querySelector("#msgTxt");
+let userScore = document.querySelector(".userScore");
+let compScore = document.querySelector(".compScore");
+let userScoreInt = parseInt(userScore.innerText);
+let compScoreInt = parseInt(compScore.innerText);
 const mainGame = (tagId) => {
   let compChoice = numGen();
   if (tagId == compChoice) {
     msgBox.innerText = "That's A Draw";
+    msgBox.style.backgroundColor = "black";
   } else {
     let userWin = true;
     if (tagId == "rock") {
       // paper , scissors
       userWin = compChoice == "scissors" ? true : false;
+      console.log("User Choice is :", tagId);
+      console.log("Comp Choice is :", compChoice);
     } else if (tagId == "paper") {
       // paper , scissors
       userWin = compChoice == "rock" ? true : false;
+      console.log("User Choice is :", tagId);
+      console.log("Comp Choice is :", compChoice);
     } else {
       userWin = compChoice == "paper" ? true : false;
+      console.log("User Choice is :", tagId);
+      console.log("Comp Choice is :", compChoice);
     }
     showWin(userWin);
   }
@@ -21,8 +32,16 @@ const mainGame = (tagId) => {
 const showWin = (userWin) => {
   if (userWin == true) {
     msgBox.innerText = "You Won ";
-  } else {
+    msgBox.style.backgroundColor = "green";
+    userScoreInt++;
+    userScore.innerText = userScoreInt;
+    console.log(userScore, userScoreInt);
+  } else if (userWin == false) {
     msgBox.innerText = "Conp Won!";
+    msgBox.style.backgroundColor = "red";
+    compScoreInt++;
+    compScore.innerText = compScoreInt;
+    console.log(compScore, compScoreInt);
   }
 };
 const numGen = () => {
