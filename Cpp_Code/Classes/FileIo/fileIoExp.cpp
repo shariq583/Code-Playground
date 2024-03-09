fea5b3315bc44d414c77d0496b9295b595194f7f
shariq583 <gazdarshariq0@gmail.com>
2024-03-09 11:18:33 +0500

    fileIoExp add!

---
+++ Cpp_Code/Classes/FileIo/fileIoExp.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+#include<fstream>
+using namespace std;
+int main()
+{
+    fstream user("fileIoExp.txt");
+    string name;
+    std::cout << "Enter some names" << name << std::endl;
+}
