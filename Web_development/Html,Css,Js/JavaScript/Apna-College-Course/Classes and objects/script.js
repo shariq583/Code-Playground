50a39c23b893f1ebf90d95e7b6ed247017501d8e
shariq583 <gazdarshariq0@gmail.com>
2024-03-14 19:06:14 +0500

    .

--- Web_development/Html,Css,Js/JavaScript/Apna-College-Course/Classes and objects/script.js
+++ Web_development/Html,Css,Js/JavaScript/Apna-College-Course/Classes and objects/script.js
@@ -1,3 +1,40 @@
-const student = {
-    marks : 99
+// const student = {
+//   calcRes(marks) {
+//     console.log("The result is :", marks);
+//   },
+// };
+// const shariq = {
+//   marks: 99,
+// };
+// shariq.__proto__ = student;
+// class car {
+//   constructor(brand) {
+//     this.brand = brand;
+//     console.log(`The brand of this car is ${brand}`);
+//   }
+//   start() {
+//     console.log("Start!");
+//   }
+//   stop() {
+//     console.log("Stop!");
+//   }
+// }
+// class bike extends car {
+//   constructor(brand) {
+//     console.log(`The brand of this bike is ${brand}`);
+//   }
+// }
+// let BMW = new bike("BMW");
+// Practice Questions
+// Q1
+class user {
+  constructor(name, email) {
+    this.name = name;
+    this.email = email;
+  }
+  viewData() {
+    console.log(`The name of user is ${this.name}`);
+    console.log(`The email of user is ${this.email}`);
+  }
 }
+let shariq = new user("Shariq", "Gazdarshariq0@gmail.com");
