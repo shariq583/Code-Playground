c4035117c6fb1c9756c97fce0fe53d342477bdc1
shariq583 <gazdarshariq0@gmail.com>
2024-04-07 18:04:47 +0500

    code restore

--- Web_development/React Course/class2/src/App.jsx
+++ Web_development/React Course/class2/src/App.jsx
@@ -1,57 +1,132 @@
-import { useState } from "react";
-import "./App.css";
-import NavBar from "./components/NavBar";
-import TextPart from "./components/TextPart";
-import Alert from "./components/Alert";
+import React, { useState } from "react";
 
-function App() {
-  const [mode, changeMode] = useState("light");
-  const [btn, setBtn] = useState("bi bi-moon-stars");
-  const [alert, setAlert] = useState(null);
-  const [color, setColor] = useState("grey");
+const About = () => {
+  const [theme, changeTheme] = useState({
+    color: "white",
+    backgroundColor: "black",
+  });
 
-  const showAlert = (message, state) => {
-    setAlert({ message: message, state: state });
-    setTimeout(() => {
-      setAlert(null);
-    }, 875);
-  };
-  const changeTheme = (newColor) => {
-    if (mode === "light") {
-      changeMode("dark");
-      setBtn("bi bi-brightness-high");
-      document.body.style.backgroundColor = newColor; // Update body background color directly
-      document.body.style.color = "white";
-      showAlert("Changed To Costume ", "Success");
+  const [btn, changeBtn] = useState("Switch to light");
+  const changeFunc = () => {
+    if (theme.color === "white") {
+      changeTheme({
+        color: "black",
+        backgroundColor: "white",
+      });
+      changeBtn("Switch to dark");
     } else {
-      changeMode("light");
-      setBtn("bi bi-moon-stars");
-      document.body.style.backgroundColor = "white"; // Update body background color directly
-      document.body.style.color = "black";
-      showAlert("Changed To Light ", "Success");
+      changeTheme({ color: "white", backgroundColor: "black" });
+      changeBtn("Switch to light");
     }
   };
 
-  const getData = (colorFunc) => {
-    setColor(colorFunc);
-    console.log(colorFunc);
-    changeTheme(colorFunc); // Call changeTheme to update the body background color
-  };
-
   return (
-    <div className="firstDiv">
-      <NavBar
-        title="Text Tool"
-        aboutTxt="About"
-        mode={mode}
-        btnStyle={btn}
-        changeFunc={changeTheme}
-        costumeTheme={getData} // Pass the callback function as a prop
-      />
-      <Alert aboutTxt={alert} />
-      <TextPart heading="Enter Your Text Below" changeFunc={showAlert} />
-    </div>
+    <>
+      <div className="container my-3" style={theme}>
+        <h1>About Us!</h1>
+        <div className="accordion" id="accordionExample" style={theme}>
+          <div className="accordion-item">
+            <h2 className="accordion-header">
+              <button
+                className="accordion-button"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#collapseOne"
+                aria-expanded="true"
+                aria-controls="collapseOne"
+                style={theme}
+              >
+                Accordion Item #1
+              </button>
+            </h2>
+            <div
+              id="collapseOne"
+              className="accordion-collapse collapse show"
+              data-bs-parent="#accordionExample"
+            >
+              <div className="accordion-body" style={theme}>
+                <strong>This is the first item's accordion body.</strong> It is
+                shown by default, until the collapse plugin adds the appropriate
+                classes that we use to style each element. These classes control
+                the overall appearance, as well as the showing and hiding via
+                CSS transitions. You can modify any of this with custom CSS or
+                overriding our default variables. It's also worth noting that
+                just about any HTML can go within the{" "}
+                <code>.accordion-body</code>, though the transition does limit
+                overflow.
+              </div>
+            </div>
+          </div>
+          <div className="accordion-item">
+            <h2 className="accordion-header">
+              <button
+                className="accordion-button collapsed"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#collapseTwo"
+                aria-expanded="false"
+                aria-controls="collapseTwo"
+                style={theme}
+              >
+                Accordion Item #2
+              </button>
+            </h2>
+            <div
+              id="collapseTwo"
+              className="accordion-collapse collapse"
+              data-bs-parent="#accordionExample"
+            >
+              <div className="accordion-body" style={theme}>
+                <strong>This is the second item's accordion body.</strong> It is
+                hidden by default, until the collapse plugin adds the
+                appropriate classes that we use to style each element. These
+                classes control the overall appearance, as well as the showing
+                and hiding via CSS transitions. You can modify any of this with
+                custom CSS or overriding our default variables. It's also worth
+                noting that just about any HTML can go within the{" "}
+                <code>.accordion-body</code>, though the transition does limit
+                overflow.
+              </div>
+            </div>
+          </div>
+          <div className="accordion-item">
+            <h2 className="accordion-header">
+              <button
+                className="accordion-button collapsed"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#collapseThree"
+                aria-expanded="false"
+                aria-controls="collapseThree"
+                style={theme}
+              >
+                Accordion Item #3
+              </button>
+            </h2>
+            <div
+              id="collapseThree"
+              className="accordion-collapse collapse"
+              data-bs-parent="#accordionExample"
+            >
+              <div className="accordion-body" style={theme}>
+                <strong>This is the third item's accordion body.</strong> It is
+                hidden by default, until the collapse plugin adds the
+                appropriate classes that we use to style each element. These
+                classes control the overall appearance, as well as the showing
+                and hiding via CSS transitions. You can modify any of this with
+                custom CSS or overriding our default variables. It's also worth
+                noting that just about any HTML can go within the{" "}
+                <code>.accordion-body</code>, though the transition does limit
+                overflow.
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="container my-3" onClick={changeFunc}>
+          <button className="btn btn-primary">{btn}</button>
+        </div>
+      </div>
+    </>
   );
-}
-
-export default App;
+};
+export default About;
