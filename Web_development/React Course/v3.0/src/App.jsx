e6d913763b8ba927cf3636e55428b785d090dfd5
shariq583 <gazdarshariq0@gmail.com>
2024-04-10 01:10:25 +0500

    router add!

--- Web_development/React Course/v3.0/src/App.jsx
+++ Web_development/React Course/v3.0/src/App.jsx
@@ -1,11 +1,6 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Routes,
-} from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 import "./App.css";
 import NavBar from "./components/NavBar";
