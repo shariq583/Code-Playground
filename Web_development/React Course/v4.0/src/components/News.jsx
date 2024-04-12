90a1a7820ca916328c9fac47d6289452747a9b2c
shariq583 <gazdarshariq0@gmail.com>
2024-04-13 00:44:49 +0500

    news app add!

--- Web_development/React Course/v4.0/src/components/News.jsx
+++ Web_development/React Course/v4.0/src/components/News.jsx
@@ -10,13 +10,13 @@ export class News extends Component {
             desc="Some description"
             imgUrl="https://cdn.vox-cdn.com/thumbor/Ll37ZgQZ1HePIrXxvnCUUdaCPMM=/0x0:2040x1360/1200x628/filters:focal(1015x801:1016x802)/cdn.vox-cdn.com/uploads/chorus_asset/file/25052488/236896_MacBook_Pro_14_and_16_M3_AKrales_0098.jpg"
           />
-          <NewsItem />
-          <NewsItem />
+          <NewsItem title="Some title" desc="Some description" />
+          <NewsItem title="Some title" desc="Some description" />
         </div>
         <div className="d-flex flex-row ">
-          <NewsItem />
-          <NewsItem />
-          <NewsItem />
+          <NewsItem title="Some title" desc="Some description" />
+          <NewsItem title="Some title" desc="Some description" />
+          <NewsItem title="Some title" desc="Some description" />
         </div>
       </div>
     );
--- Web_development/React Course/v4.0/src/components/NewsItem.jsx
+++ Web_development/React Course/v4.0/src/components/NewsItem.jsx
@@ -13,8 +13,8 @@ export default class NewsItem extends Component {
           <div class="card-body">
             <h5 class="card-title">{title}</h5>
             <p class="card-text">{desc}</p>
-            <a href="#" class="btn btn-primary">
-              Go somewhere
+            <a href="#" class="btn btn-primary btn-sm">
+              Get Details!
             </a>
           </div>
         </div>
