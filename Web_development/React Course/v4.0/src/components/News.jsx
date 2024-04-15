03fb54e4047a2e3a053ea19fd5f72264626c70b5
shariq583 <gazdarshariq0@gmail.com>
2024-04-15 12:08:48 +0500

    rp

--- Web_development/React Course/v4.0/src/components/News.jsx
+++ Web_development/React Course/v4.0/src/components/News.jsx
@@ -11,7 +11,7 @@ export class News extends Component {
 
   async componentDidMount() {
     let endpoint =
-      "https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=0a4ece348d504c7c9f16781fb19c222b";
+      "https://newsapi.org/v2/everything?q=tesla&from=2024-03-15&sortBy=publishedAt&apiKey=0a4ece348d504c7c9f16781fb19c222b";
     let result = await fetch(endpoint);
     let resultJson = await result.json();
     this.setState({ article: resultJson.articles });
