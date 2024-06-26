e43bc5978861d8bd3599c76caac94596a61398b3
shariq583 <gazdarshariq0@gmail.com>
2024-06-26 23:02:55 +0500

    nav Lap

--- Web_development/Projects/Recipe Website/temp/index.js
+++ Web_development/Projects/Recipe Website/temp/index.js
@@ -1,9 +1,10 @@
 let menuBtn = document.querySelector("#menuBtn");
 let closeBtn = document.querySelector("#closeBtn");
-
+let list = document.querySelector("#hamLinkContainer");
 menuBtn.addEventListener("click", () => {
   closeBtn.classList.remove("Hide");
   menuBtn.classList.add("Hide");
+  list.classList;
 });
 
 closeBtn.addEventListener("click", () => {
